dffcda217839b80ec3dd0a43259b321562656846
Hetu Patel <hetu200211@gmail.com>
2021-10-22 23:30:01 +0530

    room.js

--- realtime-notepad/src/components/Rooms/Room.js
+++ realtime-notepad/src/components/Rooms/Room.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Button, Modal } from "react-bootstrap";
 import "../../style/room.css";
-const socket = io('/')
+// const socket = io('/')
 const RoomData = [
   {
     name: "one",
