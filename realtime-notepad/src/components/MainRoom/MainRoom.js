d749b427b760de975fb4253b28372c44f18313a7
Hetu Patel <hetu200211@gmail.com>
2021-10-23 14:56:42 +0530

    loginsignup page

--- realtime-notepad/src/components/MainRoom/MainRoom.js
+++ realtime-notepad/src/components/MainRoom/MainRoom.js
@@ -3,10 +3,12 @@ import Notepad from './Notepad'
 import FileSystem from './FileSystem'
 import '../../style/mainRoom.css'
 import MainChat from './Chat'
+import MainNav from '../Nav'
 
 function MainRoom() {
     return (
         <div className="main_room_front_page">
+        <MainNav/>
             <FileSystem/>
             <Notepad/>
             <MainChat/>
