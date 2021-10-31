a912eb9bcac76f67c85e9612bb8233926671e1cb
zaidm124 <zaidm124@gmail.com>
2021-10-31 16:49:48 +0530

    id bug fixed

--- realtime-notepad/frontend/src/components/MainRoom/Notepad.js
+++ realtime-notepad/frontend/src/components/MainRoom/Notepad.js
@@ -1,54 +1,54 @@
-import React, { useEffect, useState } from 'react';
-import db from '../FireBase';
-import { withRouter } from 'react-router-dom';
-import VoiceRoom from '../VoiceChat/VoiceRoom';
+import React, { useEffect, useState } from "react";
+import db from "../FireBase";
+import { withRouter } from "react-router-dom";
+import VoiceRoom from "../VoiceChat/VoiceRoom";
 
-function Notepad (props) {
-    const [textArea, setTextArea] = useState('');
-    const [editorId, setEditorId] = useState('');
+function Notepad(props) {
+  const [textArea, setTextArea] = useState("");
+  const [editorId, setEditorId] = useState("");
 
-    useEffect(() => {
-        db.collection('rooms')
-        .doc(props.id)
-        .collection('files')
-        .doc(props.fileId)
-        .collection('data')
-        .onSnapshot((snap) => {
-            setEditorId(snap.docs[0]?.id);
-            setTextArea(snap.docs[0]?.data()?.message);
-            console.log('updated');
-        });
-    }, [props.fileId]);
+  useEffect(() => {
+    db.collection("rooms")
+      .doc(props.id)
+      .collection("files")
+      .doc(props.fileId)
+      .collection("data")
+      .onSnapshot((snap) => {
+        setEditorId(snap.docs[0]?.id);
+        setTextArea(snap.docs[0]?.data()?.message);
+        console.log("updated");
+      });
+  }, [props.fileId]);
 
-    const handleChange = (data) => {
-        setTextArea(data);
-        db.collection('rooms')
-        .doc(props.id)
-        .collection('files')
-        .doc(props.fileId)
-        .collection('data')
-        .doc(editorId)
-        .update({
-            message: data,
-        })
-        .then(() => {
-            console.log('added');
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-    };
+  const handleChange = (data) => {
+    setTextArea(data);
+    db.collection("rooms")
+      .doc(props.id)
+      .collection("files")
+      .doc(props.fileId)
+      .collection("data")
+      .doc(editorId)
+      .update({
+        message: data,
+      })
+      .then(() => {
+        console.log("added");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    return (
-        <div>
-            <VoiceRoom roomId = {props.location.state.id}/>
-            <textarea
-                style = {{ height: '400px', width: '500px' }}
-                value = {textArea}
-                onChange = {(e) => handleChange(e.target.value)}
-            />
-        </div>
-    );
+  return (
+    <div>
+      <VoiceRoom roomId={props.id} />
+      <textarea
+        style={{ height: "400px", width: "500px" }}
+        value={textArea}
+        onChange={(e) => handleChange(e.target.value)}
+      />
+    </div>
+  );
 }
 
 export default withRouter(Notepad);
